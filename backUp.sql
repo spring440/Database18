379d75d3942adb6515cc93e23348074ec93503a6
fayeeeeeeee <330399709@qq.com>
2017-05-14 14:19:26 -0700

    Delete backUp.sql

--- backUp.sql
+++
@@ -1,8 +0,0 @@
-USE s17guest18;
-GO
-BACKUP DATABASE s17guest18
-    TO DISK = '\s17guest18.Bak'
-      WITH FORMAT,
-        MEDIANAME = 'Z_SQLServerBackups',
-        NAME = 'Full Backup of s17guest18';
-GO
